0da97a4f11dc3719b20b22c30793776903511a5f
jinwoo <zvzv9808@gmail.com>
2023-02-05 21:11:08 +0900

    Initial Commit

---
+++ src/test/java/jinwoo/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package jinwoo.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
